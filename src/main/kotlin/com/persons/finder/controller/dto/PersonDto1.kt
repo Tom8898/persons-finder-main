d8ce0170e74a7624f8be8bd926174668781da4eb
Tom8898 <jingtao416@gmail.com>
2026-05-27 19:50:19 +1200

    feat: add create person request dto

---
+++ src/main/kotlin/com/persons/finder/controller/dto/PersonDto1.kt
@@ -0,0 +1,10 @@
+package com.persons.finder.controller.dto
+
+data class CreatePersonRequest1(
+    val name: String,
+    val title: String,
+    val hobbies: String,
+    val latitude: Double,
+    val longitude: Double
+)
+
